d11aa8f8014a0ce8edb8071a2c1eb97f97aebeb4
Jeremy Zornow <jeremy@zornow.com>
2017-04-13 13:46:19 -0600

    Remove ContactsHelper from the files included.
    It won't be used by apps using this gem. It will only be used by
    the gem itself.

--- lib/intacct_ruby.rb
+++ lib/intacct_ruby.rb
@@ -1,9 +1,8 @@
 require 'intacct_ruby/api'
 require 'intacct_ruby/version'
+
 require 'intacct_ruby/request'
 require 'intacct_ruby/response'
-
-require 'intacct_ruby/helpers/contacts_helper'
 
 require 'intacct_ruby/functions/base_function'
 
