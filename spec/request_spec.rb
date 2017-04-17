b59a64d1e4d3be1ff25ddbd4e01eefc3c069f377
Jeremy Zornow <jeremy@zornow.com>
2017-04-17 12:42:03 -0600

    Reorganize request spec
    This is an OCD thing. There were some bad describe/it groupings
    thad had been bugging me for a while. Now their output way better
    describes the way that things actually function with + without
    overrides.

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -5,6 +5,7 @@ require 'nokogiri'
 require 'intacct_ruby/request'
 require 'intacct_ruby/response'
 require 'intacct_ruby/exceptions/insufficient_credentials_exception'
+require 'intacct_ruby/exceptions/empty_request_exception'
 
 include IntacctRuby
 
@@ -48,42 +49,40 @@ def function_stubs
 end
 
 describe Request do
-  context 'with no overrides' do
-    describe :send do
-      it 'sends request through the API' do
-        request = Request.new(*function_stubs, AUTHENTICATION_PARAMS)
-        response = mock('IntacctRuby::Response')
+  describe :send do
+    it 'sends request through the API' do
+      request = Request.new(*function_stubs, AUTHENTICATION_PARAMS)
+      response = mock('IntacctRuby::Response')
 
-        api_spy = mock('IntacctRuby::Api')
-        api_spy.expects(:send).with(request).returns(response)
+      api_spy = mock('IntacctRuby::Api')
+      api_spy.expects(:send).with(request).returns(response)
 
-        Response.expects(:new).with(response)
+      Response.expects(:new).with(response)
 
-        request.send(api_spy)
-      end
+      request.send(api_spy)
+    end
 
-      it 'raises error unless all authentication keys are provided' do
-        AUTHENTICATION_PARAMS.keys.each do |omitted_key|
-          incomplete_params = AUTHENTICATION_PARAMS.dup
-          incomplete_params.delete(omitted_key)
+    it 'raises error unless all authentication keys are provided' do
+      AUTHENTICATION_PARAMS.keys.each do |omitted_key|
+        incomplete_params = AUTHENTICATION_PARAMS.dup
+        incomplete_params.delete(omitted_key)
 
-          request = Request.new(*function_stubs, incomplete_params)
+        request = Request.new(*function_stubs, incomplete_params)
 
-          expected_error = Exceptions::InsufficientCredentialsException
-          expected_message = Regexp.new(
-            "\\[:#{omitted_key}\\] required for a valid request."
-          )
+        expected_error = Exceptions::InsufficientCredentialsException
+        expected_message = Regexp.new(
+          "\\[:#{omitted_key}\\] required for a valid request."
+        )
 
-          expect { request.send }
-            .to raise_error(expected_error, expected_message)
-        end
+        expect { request.send }
+          .to raise_error(expected_error, expected_message)
       end
     end
 
-    before(:all) { generate_request_xml }
-
     describe 'control block' do
       it 'contains expected authentication parameters' do
+        generate_request_xml
+
         {
           senderid: 'senderid',
           sender_password: 'password'
@@ -96,24 +95,19 @@ describe Request do
       end
 
       it 'contains valid controlid' do
+        generate_request_xml
+
         controlid = get_value_from control_block_xml, 'controlid'
 
         # if controlid is not a valid datetime string, this will blow up
         expect { DateTime.parse(controlid) }.not_to raise_error
       end
-
-      it 'contains a control block with default values' do
-        %i(uniqueid dtdversion includewhitespace).each do |field_name|
-          expected_value = Request::DEFAULTS[field_name].to_s
-          actual_value = get_value_from control_block_xml, field_name.to_s
-
-          expect(expected_value).to eq actual_value
-        end
-      end
     end
 
     describe 'authentication block' do
       it 'contains expected authentication parameters' do
+        generate_request_xml
+
         authentication_block_xml = @request_xml.xpath(
           '/request/operation/authentication/login'
         )
@@ -131,51 +125,68 @@ describe Request do
       end
     end
 
-    describe 'operation block' do
-      it 'shows transaction default' do
-        expect(operation_block_xml.first.attributes['transaction'].value)
-          .to eq Request::DEFAULTS[:transaction].to_s
-      end
-    end
-
     describe 'content block' do
       it 'contains function payloads' do
+        generate_request_xml
+
         content_block = operation_block_xml.xpath('content').text
         function_stubs.each do |function|
           expect(content_block).to include function.to_xml
         end
       end
     end
-  end
 
-  context 'with overrides' do
-    describe 'control block' do
-      it 'shows overrides instead of defaults' do
-        overrides = {
-          uniqueid: 'uniqueid override',
-          dtdversion: 'dtdversion override',
-          includewhitespace: 'includewhitespace override'
-        }
+    context 'with no overrides' do
+      before(:all) { generate_request_xml }
 
-        generate_request_xml(overrides)
+      describe 'control block' do
+        it 'contains default values' do
+          %i(uniqueid dtdversion includewhitespace).each do |field_name|
+            expected_value = Request::DEFAULTS[field_name].to_s
+            actual_value = get_value_from control_block_xml, field_name.to_s
 
-        overrides.each do |field_name, field_value|
-          request_value = get_value_from control_block_xml, field_name.to_s
+            expect(expected_value).to eq actual_value
+          end
+        end
+      end
 
-          expect(request_value).to eq field_value
+      describe 'operation block' do
+        it 'shows transaction default' do
+          expect(operation_block_xml.first.attributes['transaction'].value)
+            .to eq Request::DEFAULTS[:transaction].to_s
         end
       end
     end
 
-    describe 'operations block' do
-      it 'shows overrides instead of defaults' do
-        transaction_override_value = 'Transaction Override'
+    context 'with overrides' do
+      describe 'control block' do
+        it 'shows overrides instead of defaults' do
+          overrides = {
+            uniqueid: 'uniqueid override',
+            dtdversion: 'dtdversion override',
+            includewhitespace: 'includewhitespace override'
+          }
 
-        generate_request_xml(transaction: transaction_override_value)
+          generate_request_xml(overrides)
 
-        request_attribute = operation_block_xml.first.attributes['transaction']
+          overrides.each do |field_name, field_value|
+            request_value = get_value_from control_block_xml, field_name.to_s
 
-        expect(request_attribute.value).to eq transaction_override_value
+            expect(request_value).to eq field_value
+          end
+        end
+      end
+
+      describe 'operations block' do
+        it 'shows overrides instead of defaults' do
+          transaction_override_value = 'Transaction Override'
+
+          generate_request_xml(transaction: transaction_override_value)
+
+          request_attribute = operation_block_xml.first.attributes['transaction']
+
+          expect(request_attribute.value).to eq transaction_override_value
+        end
       end
     end
   end
