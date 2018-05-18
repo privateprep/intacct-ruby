7d1f6524db7f176cbdc5073a54d5a78e4c68a57f
Dan <danielpowell12@msn.com>
2018-05-18 14:11:13 -0600

    Removes dead functions + helpers
    These are vestiges of a time before targeting the DTD 3.0 API at
    which time we went for a lower level style approach.
    
    No need to keep around any longer.

--- lib/intacct_ruby.rb
+++ lib/intacct_ruby.rb
@@ -3,25 +3,3 @@ require 'intacct_ruby/version'
 
 require 'intacct_ruby/request'
 require 'intacct_ruby/response'
-
-require 'intacct_ruby/functions/base_function'
-
-require 'intacct_ruby/functions/create_customer'
-require 'intacct_ruby/functions/update_customer'
-
-require 'intacct_ruby/functions/create_project'
-require 'intacct_ruby/functions/update_project'
-
-require 'intacct_ruby/functions/create_employee'
-require 'intacct_ruby/functions/update_employee'
-
-require 'intacct_ruby/functions/create_location'
-require 'intacct_ruby/functions/update_location'
-
-require 'intacct_ruby/functions/create_item'
-require 'intacct_ruby/functions/update_item'
-
-require 'intacct_ruby/functions/create_aradjustment'
-
-require 'intacct_ruby/functions/create_gltransaction'
-require 'intacct_ruby/functions/create_statgltransaction'
