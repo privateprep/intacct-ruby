8cc44c45012da12b82a5a169edb15e9cee74091f
Daniel Cox <draycox@gmail.com>
2019-11-18 22:53:23 -0600

    Optional keys on legacy endpoints
    Why:
    
    * Allow users to mark fields as optional and remove before sending.
    
    * This change will allow for nested params and will remove any keys
    marked with '*' in the key.
    
    This change addresses the need by:
    
    *  Expand the legacy function behavior
    
    Ticket
    
    * [CLOUD-724]

--- lib/intacct_ruby/legacy_function.rb
+++ lib/intacct_ruby/legacy_function.rb
@@ -3,20 +3,56 @@ module IntacctRuby
 
     def to_xml
       xml = Builder::XmlMarkup.new
-      
+      # Use the parameters to create attributes
       attributes = @parameters.select { |k,v| /^\*/.match(k) }
       attributes = attributes.inject({}) do |hash, (k, v)|
         hash[k.to_s.gsub(/^\*/, '')] = v
         hash
       end unless attributes.empty?
 
+      # Find the duplicate keys
+      duplicate = @parameters.deep_dup
+      omitted_keys = []
+
+      duplicate.each do |key, value|
+        omitted_keys << value.select { |k,v| /^\*/.match(k) } if value.is_a? Hash
+        if value.is_a? Array
+          value.each do |val|
+            if val.is_a? Hash
+              omitted_keys << val.select { |k,v| /^\*/.match(k) }
+              val.each_value do |v|
+                omitted_keys << v.select { |k,v| /^\*/.match(k) } if v.is_a? Hash
+              end
+            end
+          end
+        end
+      end
+      omitted_keys = omitted_keys.reduce({}, :merge).collect { |k,v| k.to_s.gsub(/^\*/, '').to_sym }
+
+      #  Clean the parameters before sending off to API
+      @parameters.each do |key, value|
+        value.reject! { |k,v| /^\*/.match(k) || omitted_keys.include?(k)} if value.is_a? Hash
+        if value.is_a? Array
+          value.each do |val|
+            if val.is_a? Hash
+              val.reject! { |k,v| /^\*/.match(k) || omitted_keys.include?(k)}
+              val.each_value do |v|
+                v.reject! { |k,v| /^\*/.match(k) || omitted_keys.include?(k)} if v.is_a? Hash
+              end
+            end
+          end
+        end
+      end
+
+      # Build the XML
       xml.function controlid: controlid do
         xml.tag! @object_type, attributes do
-          xml << parameter_xml(@parameters.reject { |k,v| /^\*/.match(k) })
+          xml << parameter_xml(@parameters)
         end
       end
 
       xml.target!
     end
+
   end
 end
